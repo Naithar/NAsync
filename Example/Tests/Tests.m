b799bedc6327c65f9e35894dc30cc7ca3b0b42ff
Naithar <devias.naith@gmail.com>
2015-04-19 03:08:08 +0300

    Added coveralls

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,40 +6,42 @@
 //  Copyright (c) 2014 Naithar. All rights reserved.
 //
 
+
+@import NAsync;
 SpecBegin(InitialSpecs)
 
-describe(@"these will fail", ^{
-
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
-    });
-
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
-    });
-
-//    it(@"will wait for 10 seconds and fail", ^{
-//        waitUntil(^(DoneCallback done) {
-//        
-//        });
-//    });
-});
-
-//describe(@"these will pass", ^{
-//    
+//describe(@"these will fail", ^{
+//
 //    it(@"can do maths", ^{
 //        expect(1).beLessThan(23);
 //    });
-//    
+//
 //    it(@"can read", ^{
 //        expect(@"team").toNot.contain(@"I");
 //    });
-//    
-//    it(@"will wait and succeed", ^AsyncBlock {
+//
+//    it(@"will wait for 10 seconds and fail", ^{
 //        waitUntil(^(DoneCallback done) {
 //            done();
 //        });
 //    });
 //});
+
+describe(@"operation without return type", ^{
+    it(@"will run", ^{
+        __block NSInteger index = 0;
+        waitUntil(^(DoneCallback done) {
+            [[[NAsyncOperation alloc] initWithDelay:0
+                                          priority:NSOperationQueuePriorityNormal
+                                 previousOperation:nil
+                                          andBlock:^(NAsyncOperation *operation, id value) {
+                                              index = [value integerValue];
+                                              done();
+                                          }] performOnQueue:[NSOperationQueue mainQueue] withValue:@10];
+        });
+
+        expect(index).to.equal(10);
+    });
+});
 
 SpecEnd
