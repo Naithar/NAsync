0347f1c7f4d3159903aef97a7379106c99427e12
Naithar <devias.naith@gmail.com>
2015-04-20 22:23:33 +0300

    added test for NAsyncManager queue and promise queue

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -8,47 +8,193 @@
 
 @import NAsync;
 
-
-SpecBegin(InitialSpecs)
-
+SpecBegin(NAsyncOperationBlock)
 
 describe(@"operation without return type", ^{
     it(@"will run", ^{
+        waitUntil(^(DoneCallback done) {
+            [[[NAsyncOperation alloc] initWithDelay:0
+                                           priority:NSOperationQueuePriorityNormal
+                                  previousOperation:nil
+                                           andBlock:^(NAsyncOperation *operation, id value) {
+                                               done();
+                                           }] performOnQueue:[NSOperationQueue mainQueue]];
+        });
+    });
+
+    it(@"will run and receive value", ^{
         __block NSInteger index = 0;
         waitUntil(^(DoneCallback done) {
             [[[NAsyncOperation alloc] initWithDelay:0
-                                          priority:NSOperationQueuePriorityNormal
-                                 previousOperation:nil
-                                          andBlock:^(NAsyncOperation *operation, id value) {
-                                              index = [value integerValue];
-                                              done();
-                                          }] performOnQueue:[NSOperationQueue mainQueue] withValue:@10];
+                                           priority:NSOperationQueuePriorityNormal
+                                  previousOperation:nil
+                                           andBlock:^(NAsyncOperation *operation, id value) {
+                                               index = [value integerValue];
+                                               done();
+                                           }] performOnQueue:[NSOperationQueue mainQueue] withValue:@10];
         });
 
         expect(index).to.equal(10);
     });
 });
 
+SpecEnd
+
+
+SpecBegin(NAsyncOperationReturnBlock)
+
 describe(@"operation with return value", ^{
-    it(@"will run", ^{
+    it(@"will run and return value", ^{
+        __block NAsyncOperation *operation;
+
+        waitUntil(^(DoneCallback done) {
+            operation = [[NAsyncOperation alloc] initWithDelay:0
+                                                      priority:NSOperationQueuePriorityNormal
+                                             previousOperation:nil
+                                                andReturnBlock:^(NAsyncOperation *operation, id value) {
+                                                    done();
+                                                    return @20;
+                                                }];
+            [operation performOnQueue:[NSOperationQueue mainQueue]];
+        });
+
+        expect([operation wait]).to.equal(@20);
+    });
+
+    it(@"will run, receive value and return value", ^{
         __block NSInteger index = 0;
         __block NAsyncOperation *operation;
 
         waitUntil(^(DoneCallback done) {
             operation = [[NAsyncOperation alloc] initWithDelay:0
-                                           priority:NSOperationQueuePriorityNormal
-                                  previousOperation:nil
-                                           andReturnBlock:^(NAsyncOperation *operation, id value) {
-                                               index = [value integerValue];
-                                               done();
-                                               return @20;
-                                           }];
+                                                      priority:NSOperationQueuePriorityNormal
+                                             previousOperation:nil
+                                                andReturnBlock:^(NAsyncOperation *operation, id value) {
+                                                    index = [value integerValue];
+                                                    done();
+                                                    return @20;
+                                                }];
             [operation performOnQueue:[NSOperationQueue mainQueue] withValue:@10];
         });
 
         expect(index).to.equal(10);
 
         expect([operation wait]).to.equal(@20);
+    });
+});
+
+SpecEnd
+
+
+SpecBegin(NAsyncManagerQueue)
+
+describe(@"start operation", ^{
+    it(@"will promise", ^{
+
+        waitUntil(^(DoneCallback done) {
+            NAsyncManager *manager = [NAsyncManager promiseQueue:nil
+                                                           block:^(id _0, id _1){
+                                                               done();
+                                                           }];
+
+            [manager perform];
+        });
+
+    });
+
+    it(@"will promise and receive value", ^{
+
+        __block NSInteger index = 0;
+        waitUntil(^(DoneCallback done) {
+            NAsyncManager *manager = [NAsyncManager promiseQueue:nil
+                                                           block:^(id _0, NSNumber* value){
+                                                               index = [value integerValue];
+                                                               done();
+                                                           }];
+
+            [manager performWithValue:@10];
+        });
+
+        expect(index).to.equal(10);
+    });
+
+    it(@"will run task", ^{
+        waitUntil(^(DoneCallback done) {
+            [NAsyncManager queue:nil
+                           block:^(id _0, id _1){
+                               done();
+                           }];
+        });
+    });
+});
+
+
+describe(@"operation chaining", ^{
+    it(@"will run chain task", ^{
+        waitUntil(^(DoneCallback done) {
+            [[NAsyncManager queue:nil
+                            block:^(id _0, id _1){
+
+                            }]
+             queue:nil
+             block:^(id _0, id _1){
+                 done();
+             }];
+        });
+    });
+
+    it(@"will run chain task on promise", ^{
+        waitUntil(^(DoneCallback done) {
+            NAsyncManager *promise = [NAsyncManager promiseQueue:nil
+                                                           block:^(id _0, id _1){
+
+                                                           }];
+
+            [promise queue:nil
+                     block:^(id _0, id _1){
+                         done();
+                     }];
+
+            [promise perform];
+        });
+    });
+
+    it(@"will run promise chain task on promise", ^{
+        waitUntil(^(DoneCallback done) {
+            NAsyncManager *promise = [NAsyncManager promiseQueue:nil
+                                                           block:^(id _0, id _1){
+
+                                                           }];
+            
+            NAsyncManager *chainedPromise = [promise promiseQueue:nil
+                                                            block:^(id _0, id _1){
+                                                                done();
+                                                            }];
+            
+            [promise perform];
+            [chainedPromise perform];
+        });
+    });
+
+    it(@"will run promise chain task with value on promise", ^{
+        __block NSInteger index = 0;
+        waitUntil(^(DoneCallback done) {
+            NAsyncManager *promise = [NAsyncManager promiseQueue:nil
+                                                           block:^(id _0, id _1){
+
+                                                           }];
+
+            NAsyncManager *chainedPromise = [promise promiseQueue:nil
+                                                            block:^(id _0, id value){
+                                                                index = [value integerValue];
+                                                                done();
+                                                            }];
+
+            [promise perform];
+            [chainedPromise performWithValue:@10];
+        });
+
+        expect(index).to.equal(10);
     });
 });
 
