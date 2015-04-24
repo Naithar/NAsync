f778e2ef8a615a0bf1be7448fad512d30f3b5775
Naithar <devias.naith@gmail.com>
2015-04-24 16:49:11 +0300

    removed pods folder

--- Example/Pods/Target Support Files/Pods-NAsync-NAsync/Pods-NAsync-NAsync-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NAsync_NAsync : NSObject
-@end
-@implementation PodsDummy_Pods_NAsync_NAsync
-@end
--- Example/Pods/Target Support Files/Pods-NAsync-NAsync/Pods-NAsync-NAsync-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "NAsync.h"
-#import "NAsyncManager.h"
-#import "NAsyncOperation.h"
-
-FOUNDATION_EXPORT double NAsyncVersionNumber;
-FOUNDATION_EXPORT const unsigned char NAsyncVersionString[];
-
